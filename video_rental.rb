9bbb15bd6562c52bed6299eacc9640ee2361bf7e
Carlos A <cayala.w@gmail.com>
2023-10-09 20:42:59 -0500

    refactoring: frequent renter points

--- video_rental.rb
+++ video_rental.rb
@@ -38,6 +38,10 @@ class Rental
     end
     result
   end
+
+  def frequent_renter_points
+    movie.price_code == Movie::NEW_RELEASE && days_rented > 1 ? 2 : 1
+  end
 end
 
 # class customer
@@ -58,10 +62,7 @@ class Customer
     frequent_renter_points = 0
     result = "Rental Record for #{@name}\n"
     @rentals.each do |element|
-      # add frequent renter points
-      frequent_renter_points += 1
-      # add bonus for a two day new release rental
-      frequent_renter_points += 1 if element.movie.price_code == Movie::NEW_RELEASE && element.days_rented > 1
+      frequent_renter_points += element.frequent_renter_points
       # show figures for this rental
       result += "\t#{element.movie.title}\t#{element.charge}\n"
       total_amount += element.charge
